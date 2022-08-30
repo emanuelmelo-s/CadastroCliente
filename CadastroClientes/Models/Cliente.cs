e6fe761731517dd372ff70146e60f76a4386eab5
emanuelmelo-Senac <wrdante@outlook.com>
2022-08-30 20:09:13 -0300

    model pronta

---
+++ CadastroClientes/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroClientes.Models
+{
+    public class Cliente
+    {
+        public int Id { get; private set; }
+    }
+
+}
